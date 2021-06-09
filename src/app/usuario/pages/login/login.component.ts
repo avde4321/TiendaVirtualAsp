ec1b9325aa5bc839a727183a9109138d00d6ce6a
unknown <a.v.d.e4321@gmail.com>
2021-06-09 09:59:38 -0500

    cambio

--- src/app/usuario/pages/login/login.component.ts
+++ src/app/usuario/pages/login/login.component.ts
@@ -1,3 +1,4 @@
+import { absoluteFrom } from '@angular/compiler-cli/src/ngtsc/file_system';
 import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from '../../services/usuario.service';
 
@@ -5,29 +6,37 @@ import { UsuarioService } from '../../services/usuario.service';
   selector: 'app-login',
   templateUrl: './login.component.html',
   styles: [
+    `
+  .form_login{
+    left : 36%;
+    top : 36%;
+    position : absolute;
+    trasform : traslate(-50%,-50%);
+    }
+    `
+
   ]
 })
 export class LoginComponent implements OnInit {
 
   txUser!: string
   txPass!: string
-  estadoLogin:boolean = true;
+  estadoLogin: boolean = true;
 
   constructor(private _usuriaS: UsuarioService) { }
 
   ngOnInit(): void {
   }
-  
+
   login() {
     this.estadoLogin = false
     setTimeout(() => {
-      this._usuriaS.login(this.txUser, this.txPass).subscribe(res=>{
+      this._usuriaS.login(this.txUser, this.txPass).subscribe(res => {
         this._usuriaS.guardarToken(res.data.token)
         this.txUser = '';
         this.txPass = '';
         this.estadoLogin = true
-      },(error)=>
-      {
+      }, (error) => {
         this.estadoLogin = true;
       })
     }, 2000);
